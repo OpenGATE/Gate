de3becb21942cbe1d9c50d248da0c6d26e6fe425
Julien Bert <julien.bert@univ-brest.fr>
2012-03-06 16:00:36 +0000

    first version of main GPU function

--- source/gpu/src/track_gpu.cu
+++ source/gpu/src/track_gpu.cu
@@ -1,28 +1,32 @@
-#include "mc_fun_pet.cu"
+#include "fun_gpu.cu"
+#include "GateSourceGPUVoxellizedIO.hh"
 
-int mainCUDA(int argc, char* argv[]) {
+void GateGPUGeneratePrimaries(const GateSourceGPUVoxellizedInput * input, 
+                              GateSourceGPUVoxellizedOutput & output) {
+	
+}
 
-	/******************************************************************
-	 * INIT
-	 ******************************************************************/
+/*
 
-	int positron = 5000000;   // positron generated (nb gamma = 2*ptot) 
-	unsigned short int most_att_mat = 7; // 1 Water  -  7 RibBone
+	int positron = input->nb_events / 2.0f; // positron generated (nb gamma = 2*ptot) 
+	unsigned short int most_att_mat = input->phantom_most_attenuate_material; // 1 Water  -  7 RibBone
+
 	// Energy
-	float E = 0.511; // 511 keV
-	int seed = 10;
+	float E = input->E; // 511 keV
+	long seed = input->seed;
 
 	// Define phantom
 	int3 dim_phantom;
-	dim_phantom.z = 46;       // vox
-	dim_phantom.y = 63;       // vox
-	dim_phantom.x = 128;      // vox
-	float size_voxel = 4.0f;  // mm
-	const char* phantom_name = "ncat_12mat_128x63x46.bin";
-	const char* activities_val_name = "ncat_act.bin";
-	const char* activities_ind_name = "ncat_ind.bin";
+	dim_phantom.z = input->phantom_size_z; // vox
+	dim_phantom.y = input->phantom_size_y; // vox
+	dim_phantom.x = input->phantom_size_x; // vox
+	float size_voxel = input->phantom_spacing; // mm
+	//const char* phantom_name = "ncat_12mat_128x63x46.bin";
+	//const char* activities_val_name = "ncat_act.bin";
+	//const char* activities_ind_name = "ncat_ind.bin";
+
 	// Select a GPU
-	cudaSetDevice(1);
+	cudaSetDevice(0);
 
 	// Vars
 	int gamma_sim = 0;	
@@ -54,18 +58,32 @@ int mainCUDA(int argc, char* argv[]) {
 	kernel_brent_init<<<grid, threads>>>(stackgamma1);
 	kernel_brent_init<<<grid, threads>>>(stackgamma2);
 
-	// Load phantom
-	load_phantom_in_tex(phantom_name, dim_phantom);
-	// load activities
-	int nb_act = get_nb_active_voxel(activities_val_name);
-	load_activities_in_tex(activities_val_name, activities_ind_name, nb_act);
+	// Load phantom in texture mem
+	int nb = dim_phantom.z * dim_phantom.y * dim_phantom.x;
+	unsigned int mem_phantom = nb * sizeof(unsigned short int);
+	unsigned short int* dphantom;
+	cudaMalloc((void**) &dphantom, mem_phantom);
+	cudaMemcpy(dphantom, input->phantom_phantom_data, mem_phantom, cudaMemcpyHostToDevice);
+	cudaBindTexture(NULL, tex_phantom, dphantom, mem_phantom);
 
-	/************************************************************************
-	 *  GPU RUN
-	 ************************************************************************/
+	// load activities in texture mem
+	unsigned int mem_act_f = nb * sizeof(float);
+	float* activities = &(input->phantom_activity_data[0]); // &phan[0]   !!! check std:vector
+	float* dactivities;
+	cudaMalloc((void**) &dactivities, mem_act_f);
+	cudaMemcpy(dactivities, activities, mem_act_f, cudaMemcpyHostToDevice);
+	cudaBindTexture(NULL, tex_act_val, dactivities, mem_act_f);
+
+	// load activities indices in the texture mem
+	unsigned int* index = &(input->phantom_activity_index[0]); // &ind[0]  !!! check std:vector
+	unsigned int* dindex; // FIXME long
+	cudaMalloc((void**) &dindex, mem_act_i);
+	cudaMemcpy(dindex, , mem_act_i, cudaMemcpyHostToDevice);
+	cudaBindTexture(NULL, tex_act_ind, dindex, mem_act_i);
+
 
 	// Generation
-	kernel_voxelized_source_b2b<<<grid, threads>>>(stackgamma1, stackgamma2, nb_act, dim_phantom, E, size_voxel);
+	kernel_voxelized_source_b2b<<<grid, threads>>>(stackgamma1, stackgamma2, dim_phantom, E, size_voxel);
 	cudaThreadSynchronize();
 	
 	// Main loop
@@ -86,17 +104,43 @@ int mainCUDA(int argc, char* argv[]) {
 		// Check simu
 		get_nb_particles_simulated(stackgamma1, stackgamma2, phasespace1, phasespace2, &gamma_sim);		
 		cudaThreadSynchronize();
-		
-		printf("Tot particles simulated: %i\n", gamma_sim);
-		
 	} // while
 
+
+	int i=0;
+	while (i<gamma_max_sim) {
+		if (phasespace1.live[i]) {
+			GateSourceGPUVoxellizedOutputParticle particle;
+			particle.E = phasespace1.E[i];
+			particle.dx = phasespace1.dx[i];
+			particle.dy = phasespace1.dy[i];
+			particle.dz = phasespace1.dz[i];
+			particle.px = phasespace1.px[i];
+			particle.py = phasespace1.py[i];
+			particle.pz = phasespace1.pz[i];
+			particle.t = phasespace1.t[i];
+			output.push_back(particle);
+		}
+		if (phasespace2.live[i]) {
+			GateSourceGPUVoxellizedOutputParticle particle;
+			particle.E = phasespace2.E[i];
+			particle.dx = phasespace2.dx[i];
+			particle.dy = phasespace2.dy[i];
+			particle.dz = phasespace2.dz[i];
+			particle.px = phasespace2.px[i];
+			particle.py = phasespace2.py[i];
+			particle.pz = phasespace2.pz[i];
+			particle.t = phasespace2.t[i];
+			output.push_back(particle);
+		}
+		++i;
+	}
+	
 	cudaThreadExit();
 	free_device_stackgamma(stackgamma1);
 	free_device_stackgamma(stackgamma2);
 	free_host_stackgamma(phasespace1);
 	free_host_stackgamma(phasespace2);
-	
-    return 0;
 }
 
+*/
