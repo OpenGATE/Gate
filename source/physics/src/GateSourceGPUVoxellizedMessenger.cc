8edc41e7bf5d2d68818fa82a315ee863c3fe2850
Pierre Gueth <pierre.gueth@creatis.insa-lyon.fr>
2012-03-06 16:57:16 +0000

    messenger verbose

--- source/physics/src/GateSourceGPUVoxellizedMessenger.cc
+++ source/physics/src/GateSourceGPUVoxellizedMessenger.cc
@@ -14,7 +14,8 @@
 GateSourceGPUVoxellizedMessenger::GateSourceGPUVoxellizedMessenger(GateSourceGPUVoxellized* source)
 : GateSourceVoxellizedMessenger(source), m_gpu_source(source)
 { 
-	m_attach_to_cmd = new G4UIcmdWithAString((GetDirectoryName()+"attachTo").c_str(),this);
+	G4cout << "***********************" << GetDirectoryName() << G4endl;
+	m_attach_to_cmd = new G4UIcmdWithAString((GetDirectoryName()+"attachPhantomTo").c_str(),this);
 	m_attach_to_cmd->SetGuidance("Attach to gate volume");
 
 }
