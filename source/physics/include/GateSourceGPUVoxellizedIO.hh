ad497137a2ba40738cc6611dfef0c2e646ae79ba
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2012-03-30 06:33:15 +0000

    Add timeID and remove previous timing

--- source/physics/include/GateSourceGPUVoxellizedIO.hh
+++ source/physics/include/GateSourceGPUVoxellizedIO.hh
@@ -17,23 +17,23 @@
 #include <vector>
 
 struct GateSourceGPUVoxellizedInput {
-	long nb_events;
-	double E; // MeV
-	unsigned int seed;
-    double startTime;
+  long nb_events;
+  double E; // MeV
+  unsigned int seed;
+  int firstInitialID; // The ID of each particle will start from this number
 
-	// Phantom and activity map (same size)
-	// Coordinate system : 0,0,0 corner 
-	int phantom_size_x;
-	int phantom_size_y;
-	int phantom_size_z;
-    float phantom_spacing; // mm // to be change into spacingx spacingy spacingz FIXME
-    std::vector<unsigned short int> phantom_material_data; // value = ID material CONSTANT (mc_cst_pet.cu l550)
+  // Phantom and activity map (same size)
+  // Coordinate system : 0,0,0 corner 
+  int phantom_size_x; 
+  int phantom_size_y;
+  int phantom_size_z;
+  float phantom_spacing; // mm // to be change into spacingx spacingy spacingz FIXME
+  std::vector<unsigned short int> phantom_material_data; // value = ID material CONSTANT (mc_cst_pet.cu l550)
 
-	std::vector<float> activity_data; // no unit (relative) FIXME ? GATE PROCESS ????
-	std::vector<unsigned int> activity_index; 
-	// GPU (later) : ID gpu, nb thread/block
-	// Physics (later) : 
+  std::vector<float> activity_data; // no unit (relative) FIXME ? GATE PROCESS ????
+  std::vector<unsigned int> activity_index; 
+  // GPU (later) : ID gpu, nb thread/block
+  // Physics (later) : 
 };
 
 typedef std::map<std::vector<int>,double> ActivityMap;
@@ -43,25 +43,26 @@ void GateSourceGPUVoxellizedInput_delete(GateSourceGPUVoxellizedInput* input);
 void GateSourceGPUVoxellizedInput_parse_activities(const ActivityMap& activities, GateSourceGPUVoxellizedInput* input);
 
 struct GateSourceGPUVoxellizedOutputParticle {
-	float E;  // MeV
-	float dx; // direction (unary)
-	float dy; // direction (unary)
-	float dz; // direction (unary)
-	float px; // mm position
-	float py; // mm position
-	float pz; // mm position
-	float t;  // ns time
+  float E;  // MeV
+  float dx; // direction (unary)
+  float dy; // direction (unary)
+  float dz; // direction (unary)
+  float px; // mm position
+  float py; // mm position
+  float pz; // mm position
+  float t;  // ns time
+  int   initialID; // initial ID of the particle
 };
 
 struct GateSourceGPUVoxellizedOutput { 
-	typedef std::list<GateSourceGPUVoxellizedOutputParticle> ParticlesList;
-	ParticlesList particles;
-    double deltaTime;
+  typedef std::list<GateSourceGPUVoxellizedOutputParticle> ParticlesList;
+  ParticlesList particles;
+  double deltaTime;
 };
 
 
 // Main function that lunch GPU calculation
 void GateGPUGeneratePrimaries(const GateSourceGPUVoxellizedInput * input, 
-		GateSourceGPUVoxellizedOutput & output);
+                              GateSourceGPUVoxellizedOutput & output);
 
 #endif
