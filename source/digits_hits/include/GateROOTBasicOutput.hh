a506ef133b0d223255810a691bf54efc404bd61f
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2012-05-11 11:07:08 +0000

    Add Configuration.h

--- source/digits_hits/include/GateROOTBasicOutput.hh
+++ source/digits_hits/include/GateROOTBasicOutput.hh
@@ -36,6 +36,7 @@ See GATE/LICENSE.txt for further details
 #include "G4Event.hh"
 #include "G4VProcess.hh"
 #include "GateUserActions.hh"
+#include "GateConfiguration.h"
 
 class GateVVolume;
 class GateCrystalHit;
