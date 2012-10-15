7f0b467f4caef56c74085fe5bcb0fc8ceda93e85
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2012-10-15 13:13:16 +0000

    needed by TLE actor

---
+++ source/digits_hits/include/GateMaterialMuHandler.hh
@@ -0,0 +1,48 @@
+/*----------------------
+   GATE version name: gate_v6
+
+   Copyright (C): OpenGATE Collaboration
+
+This software is distributed under the terms
+of the GNU Lesser General  Public Licence (LGPL)
+See GATE/LICENSE.txt for further details
+----------------------*/
+
+
+/*!
+  \class  GateMaterialMuHandler
+  \author fabien.baldacci@creatis.insa-lyon.fr
+ */
+
+#ifndef GATEMATERIALMUHANDLER_HH
+#define GATEMATERIALMUHANDLER_HH
+
+#include "G4UnitsTable.hh"
+#include "GateMuTables.hh"
+#include "G4Material.hh"
+#include <map>
+using std::map;
+using std::string;
+
+class GateMaterialMuHandler
+{
+
+public:
+  GateMaterialMuHandler(int nbOfElements);
+  ~GateMaterialMuHandler();
+  void AddMaterial(G4Material* material);
+  double GetAttenuation(G4Material* material, double energy);
+  double GetMu(G4Material* material, double energy);
+
+private:
+  map<G4String, GateMuTable*> mMaterialTable;
+  GateMuTable** mElementsTable;
+  int mNbOfElements;
+  
+  void ReadElementFile(int z);
+  void InitElementTable();
+
+};
+
+
+#endif
