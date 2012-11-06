923f4d8f727f309591cd7cda05fadde62519ca5f
David Sarrut <david.sarrut@gmail.com>
2012-11-06 14:54:40 +0100

    Add call to mother GateVActor::SaveData

--- source/digits_hits/src/GateProductionActor.cc
+++ source/digits_hits/src/GateProductionActor.cc
@@ -1,23 +1,22 @@
 /*----------------------
-   GATE version name: gate_v6
+  GATE version name: gate_v6
 
-   Copyright (C): OpenGATE Collaboration
+  Copyright (C): OpenGATE Collaboration
 
-This software is distributed under the terms
-of the GNU Lesser General  Public Licence (LGPL)
-See GATE/LICENSE.txt for further details
-----------------------*/
+  This software is distributed under the terms
+  of the GNU Lesser General  Public Licence (LGPL)
+  See GATE/LICENSE.txt for further details
+  ----------------------*/
 
 /*
   \brief Class GateProductionActor : 
   \brief compute production count for filtered particles
- */
+*/
 
 #ifndef GATEPRODUCTIONACTOR_CC
 #define GATEPRODUCTIONACTOR_CC
 
 #include "GateProductionActor.hh"
-
 #include "GateMiscFunctions.hh"
 
 //-----------------------------------------------------------------------------
@@ -28,6 +27,7 @@ GateProductionActor::GateProductionActor(G4String name, G4int depth) :
   GateMessage("Actor",2,"GateProductionActor -- constructor"<<G4endl);
   pMessenger = new GateProductionActorMessenger(this);
 }
+//-----------------------------------------------------------------------------
 
 
 //-----------------------------------------------------------------------------
@@ -37,6 +37,7 @@ GateProductionActor::~GateProductionActor()
   GateMessage("Actor",2,"GateProductionActor -- destructor"<<G4endl);
   delete pMessenger;
 }
+//-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 /// Construct
@@ -55,18 +56,23 @@ void GateProductionActor::Construct()
   // Enable callbacks
   EnablePreUserTrackingAction(true);
 }
+//-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 // Save production image
 void GateProductionActor::SaveData()
 {
+  GateVActor::SaveData();
   GateMessage("Actor",2,"GateProductionActor -- saving filename="<< mSaveFilename <<G4endl);
   mImage.Write(mSaveFilename);
 }
+//-----------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------
 void GateProductionActor::ResetData() {
   mImage.Fill(0);
 }
+//-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
 /// Start of track callback
@@ -76,5 +82,6 @@ void GateProductionActor::UserPreTrackActionInVoxel(const int index, const G4Tra
   assert(index>=0 && index<mImage.GetNumberOfValues());
   mImage.GetValue(index)++;
 }
+//-----------------------------------------------------------------------------
 
 #endif /* end #define GATEPRODUCTIONACTOR_CC */
