cade49abb3867327e66bb0b24eb438ab021d928e
Edward Romero <edward.romero@creatis.insa-lyon.fr>
2012-12-28 12:32:16 +0100

    Actors capable to write the differential cross section for rayleigh and compton scattering process as function of scattering angle; using to this the form factor and the scattering function respectivly from the Geant 4 data base. The actor entrence are: Angle, energy, and material.

---
+++ source/digits_hits/include/GateDiffCrossSectionActorMessenger.hh
@@ -0,0 +1,44 @@
+/*----------------------
+   GATE version name: gate_v6
+
+   Copyright (C): OpenGATE Collaboration
+
+This software is distributed under the terms
+of the GNU Lesser General  Public Licence (LGPL)
+See GATE/LICENSE.txt for further details
+----------------------*/
+
+/*
+  \class  GateDiffCrossSectionActorMessenger
+  \author edward.romero@creatis.insa-lyon.fr
+*/
+
+#ifndef GATEDiffCrossSectionACTORMESSENGER_HH
+#define GATEDiffCrossSectionACTORMESSENGER_HH
+
+#include "GateActorMessenger.hh"
+//#include "G4UIcmdWithADoubleAndUnit.hh"
+class G4UIcmdWithADoubleAndUnit;
+class GateDiffCrossSectionActor;
+class GateDiffCrossSectionActorMessenger : public GateActorMessenger
+{
+public:
+  GateDiffCrossSectionActorMessenger(GateDiffCrossSectionActor* sensor);
+  virtual ~GateDiffCrossSectionActorMessenger();
+
+  void BuildCommands(G4String base);
+  void SetNewValue(G4UIcommand*, G4String);
+
+protected:
+  GateDiffCrossSectionActor * pDiffCrossSectionActor;
+
+  G4UIcmdWithADoubleAndUnit* pSetEnergyCmd;
+  G4UIcmdWithADoubleAndUnit* pSetAngleCmd;
+  G4UIcmdWithAString* pReadListEnergyCmd;
+  G4UIcmdWithAString* pReadListAngleCmd;
+  G4UIcmdWithAString * pSetMaterialCmd;
+  G4UIcmdWithAString * pReadMaterialListCmd;
+
+};
+
+#endif
