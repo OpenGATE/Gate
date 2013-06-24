1ff8fbacb9adb4c4f43f38d1d2901b80419c49f3
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-06-24 21:41:57 +0200

    Add new files to the git index and append to CMakeLists.txt

--- examples/AddExternalData.sh
+++ examples/AddExternalData.sh
@@ -12,7 +12,13 @@ then
 else
   echo $(md5sum $1 | cut -f 1-1 -d ' ') >${1}.md5
 fi
+git add ${1}.md5
+
 echo "/$(basename $1).md5-stamp" >> $(dirname $1)/.gitignore
 echo "/$(basename $1)"           >> $(dirname $1)/.gitignore
+git add $(dirname $1)/.gitignore
+
 rm $1
+
+echo "GateAddExampleData(\"DATA{${1#$(dirname $0)/}}\")" >> CMakeLists.txt
 
