5b2d6a8882cff7a9e58f7aa7b18ff2a7e362a97a
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-09-23 17:29:09 +0200

    Fixed and simplified the code for writing scatter output

--- source/digits_hits/src/GateHybridForcedDetectionActor.cc
+++ source/digits_hits/src/GateHybridForcedDetectionActor.cc
@@ -617,7 +617,6 @@ void GateHybridForcedDetectionActor::SaveData()
   imgWriter = itk::ImageFileWriter<InputImageType>::New();
   char filename[1024];
   G4int rID = G4RunManager::GetRunManager()->GetCurrentRun()->GetRunID();
-  unsigned int secondary = 0;
 
   if(mPrimaryFilename != "") {
     // Normalize by the number of particles
@@ -682,7 +681,6 @@ void GateHybridForcedDetectionActor::SaveData()
   }
 
   if(mComptonFilename != "") {
-    secondary+=1;
     sprintf(filename, mComptonFilename.c_str(), rID);
     imgWriter->SetFileName(filename);
     imgWriter->SetInput(mComptonImage);
@@ -698,7 +696,6 @@ void GateHybridForcedDetectionActor::SaveData()
   }
 
   if(mRayleighFilename != "") {
-    secondary+=2;
     sprintf(filename, mRayleighFilename.c_str(), rID);
     imgWriter->SetFileName(filename);
     imgWriter->SetInput(mRayleighImage);
@@ -714,7 +711,6 @@ void GateHybridForcedDetectionActor::SaveData()
   }
 
   if(mFluorescenceFilename != "") {
-    secondary+=4;
     sprintf(filename, mFluorescenceFilename.c_str(), rID);
     imgWriter->SetFileName(filename);
     imgWriter->SetInput(mFluorescenceImage);
@@ -729,79 +725,60 @@ void GateHybridForcedDetectionActor::SaveData()
     }
   }
 
-  if(mSecondaryFilename != "") {
-    // The secondary image contains all calculated scatterings (Compton, Rayleigh and/or Fluorescence)
+  if(mSecondaryFilename != "" || mTotalFilename != "") {
+    // The secondary image contains all calculated scatterings
+    // (Compton, Rayleigh and/or Fluorescence)
     // Create projections image
+    InputImageType::Pointer mSecondaryImage;
     mSecondaryImage = CreateVoidProjectionImage();
+
     // Add Image Filter used to sum the different figures obtained on each process
     typedef itk::AddImageFilter <OutputImageType, OutputImageType, OutputImageType> AddImageFilterType;
     AddImageFilterType::Pointer addFilter = AddImageFilterType::New();
+    addFilter->InPlaceOn();
+    addFilter->SetInput1(mSecondaryImage);
 
-    switch (secondary) {
-    case 1: // Compton
-      mSecondaryImage = mComptonImage;
-      break;
-    case 2: // Rayleigh
-      mSecondaryImage = mRayleighImage;
-      break;
-    case 3: // Rayleigh + Compton
-      addFilter->SetInput1(mComptonImage);
-      addFilter->SetInput2(mRayleighImage);
-      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-      mSecondaryImage = addFilter->GetOutput();
-      break;
-    case 4: // Fluorescence
-      mSecondaryImage = mFluorescenceImage;
-      break;
-    case 5: // Fluorescence + Compton
-      addFilter->SetInput1(mComptonImage);
-      addFilter->SetInput2(mFluorescenceImage);
-      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-      mSecondaryImage = addFilter->GetOutput();
-      break;
-    case 6: // Fluorescence + Rayleigh
-      addFilter->SetInput1(mRayleighImage);
-      addFilter->SetInput2(mFluorescenceImage);
-      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-      mSecondaryImage = addFilter->GetOutput();
-      break;
-    case 7: // Fluorescence + Rayleigh + Compton
-      addFilter->SetInput1(mRayleighImage);
-      addFilter->SetInput2(mFluorescenceImage);
-      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-      addFilter->SetInput1(addFilter->GetOutput());
-      addFilter->SetInput2(mComptonImage);
-      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-      mSecondaryImage = addFilter->GetOutput();
-      break;
-    default: // error?
-      std::cerr << "ERROR : secondary type  (" << secondary << ") does not match with any known option." << std::endl;
-      exit(-1);
-    }
+    // Compton
+    addFilter->SetInput2(mComptonImage);
+    TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
+    mSecondaryImage = addFilter->GetOutput();
+
+    // Rayleigh
+    mSecondaryImage->DisconnectPipeline();
+    addFilter->SetInput1(mSecondaryImage);
+    addFilter->SetInput2(mRayleighImage);
+    TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
+    mSecondaryImage = addFilter->GetOutput();
+
+    // Fluorescence
+    mSecondaryImage->DisconnectPipeline();
+    addFilter->SetInput1(mSecondaryImage);
+    addFilter->SetInput2(mFluorescenceImage);
+    TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
+    mSecondaryImage = addFilter->GetOutput();
 
     // Write Scattering Image
-    sprintf(filename, mSecondaryFilename.c_str(), rID);
-    imgWriter->SetFileName(filename);
-    imgWriter->SetInput(mSecondaryImage);
-    TRY_AND_EXIT_ON_ITK_EXCEPTION(imgWriter->Update());
-  }
+    if(mSecondaryFilename != "") {
+      sprintf(filename, mSecondaryFilename.c_str(), rID);
+      imgWriter->SetFileName(filename);
+      imgWriter->SetInput(mSecondaryImage);
+      TRY_AND_EXIT_ON_ITK_EXCEPTION(imgWriter->Update());
+    }
 
-  if(mTotalFilename != "") {
-    // The Total image contains the Primary + Secondary images
-    // Create projections image
-    mTotalImage = CreateVoidProjectionImage();
-    // Add Image Filter used to sum the primary image + secondary image
-    typedef itk::AddImageFilter <OutputImageType, OutputImageType, OutputImageType> AddImageFilterType;
-    AddImageFilterType::Pointer addFilter = AddImageFilterType::New();
-    addFilter->SetInput1(mSecondaryImage);
-    addFilter->SetInput2(mPrimaryImage);
-    TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
-    mTotalImage = addFilter->GetOutput();
-    // Write Total Image
-    sprintf(filename, mTotalFilename.c_str(), rID);
-    imgWriter->SetFileName(filename);
-    imgWriter->SetInput(mTotalImage);
-    TRY_AND_EXIT_ON_ITK_EXCEPTION(imgWriter->Update());
+    if(mTotalFilename != "") {
+      // Primary
+      mSecondaryImage->DisconnectPipeline();
+      addFilter->SetInput1(mSecondaryImage);
+      addFilter->SetInput2(mPrimaryImage);
+      TRY_AND_EXIT_ON_ITK_EXCEPTION( addFilter->Update() );
+      mSecondaryImage = addFilter->GetOutput();
+
+      // Write Total Image
+      sprintf(filename, mTotalFilename.c_str(), rID);
+      imgWriter->SetFileName(filename);
+      imgWriter->SetInput(mSecondaryImage);
+      TRY_AND_EXIT_ON_ITK_EXCEPTION(imgWriter->Update());
+    }
   }
 
   if(mSingleInteractionFilename!="") {
