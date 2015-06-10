1eb1b5c5afcff154a7f12d1316bc06197b869581
Alex Vergara Gil <alexvergaragil@gmail.com>
2015-06-10 16:07:39 -0400

    Making Gate Lists iterable and with vector functionalities    Modified GateObjectChildList as example of using mew functionalities    The ugly and inefficient GetElement like functions became deprecated!

--- source/general/include/GateListManager.hh
+++ source/general/include/GateListManager.hh
@@ -27,13 +27,29 @@ class GateListManager : public GateClockDependent
     		    G4bool acceptNewElements=true);
     virtual ~GateListManager();
 
+    //Adding vector capabilities to lists
+    typedef std::vector<GateNamedObject*> GateListOfNamedObject;
+    typedef GateListOfNamedObject::iterator iterator;
+    typedef GateListOfNamedObject::const_iterator const_iterator;
+    iterator begin(){ return theListOfNamedObject.begin(); }
+    iterator end(){ return theListOfNamedObject.end(); }
+    const_iterator begin() const { return theListOfNamedObject.begin(); }
+    const_iterator end() const { return theListOfNamedObject.end(); }
+
   public:
      virtual  void TheListElements(size_t indent=0) const;
      //virtual  void ListElements(size_t indent=0) const;
      
      virtual GateNamedObject* FindElement(const G4String& name);
+     //instead of this (shall be deprecated)
      virtual inline GateNamedObject* GetElement(size_t i)
       	  {return (i<theListOfNamedObject.size()) ? theListOfNamedObject[i] : 0; }
+     //We can use the following
+     GateNamedObject* operator[](size_t i)
+          {return (i<theListOfNamedObject.size()) ? theListOfNamedObject[i] : 0; }
+     const GateNamedObject* operator[](size_t i) const
+          {return (i<theListOfNamedObject.size()) ? theListOfNamedObject[i] : 0; }
+
      virtual inline size_t size() const
       	  {return theListOfNamedObject.size(); }
      virtual inline GateNamedObject* FindElementByBaseName(const G4String& baseName)
@@ -51,13 +67,17 @@ class GateListManager : public GateClockDependent
     //! Method overloading GateNamedObject::Describe()
     //! Print-out a description of the object
     virtual void Describe(size_t indent=0);
+
+    //extra vector functionalities
+    void push_back(GateNamedObject* GNO) { theListOfNamedObject.push_back(GNO); }
+    void pop_back() { theListOfNamedObject.pop_back(); }
+    inline G4bool empty() { return theListOfNamedObject.empty(); }
      
 
   protected:
     G4String mElementTypeName;
     G4bool   bAcceptNewElements;
     
-    typedef std::vector<GateNamedObject*> GateListOfNamedObject;
     GateListOfNamedObject   theListOfNamedObject;
 };
 
