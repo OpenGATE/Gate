1044e19d47b9e97db9cc7201d3bb4b3c4998e30b
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2015-07-21 15:43:42 +0200

    Added the first real test with mac/gamma.mac and benchRT reference files.

--- benchmarks/testing.sh
+++ benchmarks/testing.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 echo "Hello Gater!"
 pwd
+echo $1
+echo $2
 
 echo $GATE_BINARY
 echo $BENCHMARKS_DIRECTORY
+
+cd $BENCHMARKS_DIRECTORY/$1/
+pwd
+ls
+ls *
+
+$GATE_BINARY mac/$2.mac
+
+ls *
+
+cd reference
+tar xvzf /$1-reference.tgz
+cd ..
+
+diff reference output
