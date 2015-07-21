e7ec220bca1879efbba3c4ea1b8af0bf9fc37b05
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2015-07-21 12:04:18 +0200

    Added the first hellotest to the dashboard.

---
+++ benchmarks/testing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello Gater!"
+pwd
+
+echo $1
+echo $2
+echo $3
+echo $4
+echo $PATH
+echo $GATE_BINARY
+echo $BENCHMARKS_DIRECTORY
+echo $CTEST_BINARY_DIRECTORY
+echo ${CTEST_BINARY_DIRECTORY}
