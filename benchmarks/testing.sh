9836a5fafdd5b03ca29561e11ac4563951f1cd0b
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2015-07-21 12:39:15 +0200

    Removed testing stuff.

--- benchmarks/testing.sh
+++ benchmarks/testing.sh
@@ -2,12 +2,5 @@
 echo "Hello Gater!"
 pwd
 
-echo $1
-echo $2
-echo $3
-echo $4
-echo $PATH
 echo $GATE_BINARY
 echo $BENCHMARKS_DIRECTORY
-echo $CTEST_BINARY_DIRECTORY
-echo ${CTEST_BINARY_DIRECTORY}
