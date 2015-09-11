99f593cbc28976e4c381e49ed3ecc897d4617a85
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2015-09-11 17:43:28 +0200

    Formatted test output.

--- benchmarks/testing.sh
+++ benchmarks/testing.sh
@@ -80,8 +80,8 @@ echo "Meaning of these partial exit_status:"
 echo "'0': no difference i.e. SUCCESSFUL TEST"
 echo "'1': missing file or difference in a text file i.e. FAILING TEST"
 echo "'2': difference on a binary file i.e. FAILING TEST"
-
-
+echo
+echo
 exit_status_final=$(($exit_status_folder+$exit_status_stat))
 echo "exit_status_final=exit_status_folder + exit_status_stat is:"
 echo $exit_status_final
