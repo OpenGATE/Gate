f5a08194c6d8fd7b49b7171e9444a0318f3fbfdc
Claire Mouton <claire.mouton@creatis.insa-lyon.fr>
2015-09-11 10:35:37 +0200

    Fixed a bug.

--- benchmarks/testing.sh
+++ benchmarks/testing.sh
@@ -42,8 +42,8 @@ mkdir excluded_from_test
 mv output/BenchAnalyse.C excluded_from_test
 mv output/output-gamma-Edep.mhd excluded_from_test
 mv output/output-gamma-Edep.raw excluded_from_test
-mv output/stat-gamma.txt excluded_from_test
-mv output/README excluded_from_test
+mv reference/stat-gamma.txt excluded_from_test
+mv reference/README excluded_from_test
 mv reference/benchRT-reference.tgz excluded_from_test
 mv reference/benchRT-reference.tgz.md5 excluded_from_test
 mv reference/benchRT-reference.tgz.md5-stamp excluded_from_test
