95f02c9c34cb686b874367e394bed2814352c95f
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2015-09-21 18:58:02 +0200

    When removing noise from primary, one must still account for the detector response

--- source/digits_hits/src/GateHybridForcedDetectionActor.cc
+++ source/digits_hits/src/GateHybridForcedDetectionActor.cc
@@ -280,8 +280,15 @@ void GateHybridForcedDetectionActor::BeginOfRunAction(const G4Run*r)
     mFlatFieldImage = CreateVoidProjectionImage();
     primaryProjector->SetInput(mFlatFieldImage);
     primaryProjector->SetInput(1, flatFieldSource->GetOutput() );
-    // Removed noise from I0. 
-    primaryProjector->GetProjectedValueAccumulation().SetNumberOfPrimaries(0.);
+
+    // Remove noise from I0.
+    if(mNoisePrimary != 0) {
+      for(unsigned int i=0; i<h.GetVectorLength(); i++) {
+        energyWeightList.back() *= mEnergyResponseDetector(energyList.back());
+      }
+      primaryProjector->GetProjectedValueAccumulation().SetNumberOfPrimaries(0.);
+    }
+
     TRY_AND_EXIT_ON_ITK_EXCEPTION(primaryProjector->Update());
     mFlatFieldImage = primaryProjector->GetOutput();
   }
