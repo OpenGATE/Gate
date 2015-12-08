f1d14392b4a36f3f63dceda7e8e042071768b4f9
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2015-12-08 10:49:56 +0100

    Removed example_hybrid

--- examples/example_Hybrid/data/cylinder.sh
+++
@@ -1,7 +0,0 @@
-rtkdrawgeometricphantom \
-    --phantomfile cylinder.txt \
-    --dimension 256 \
-    --spacing 1\
-    --output cylinder.mha
-clitkImageConvert -i cylinder.mha -o cylinder.mha -c
-
--- examples/example_Hybrid/data/sphere.sh
+++
@@ -1,7 +0,0 @@
-rtkdrawgeometricphantom \
-    --phantomfile sphere.txt \
-    --dimension 256 \
-    --spacing 1\
-    --output sphere.mha
-clitkImageConvert -i sphere.mha -o sphere.mha -c
-
