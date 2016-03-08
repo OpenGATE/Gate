ac604ed438a8c6bfba14bd15d7fa38f7da1ee47c
ylemarechal <yannick.lemarechal@gmail.com>
2016-03-08 11:34:34 +0100

    comment corrections

--- source/digits_hits/include/GateImageDeformActor.hh
+++ source/digits_hits/include/GateImageDeformActor.hh
@@ -22,10 +22,22 @@
 #include "GateClock.hh"
 #include "GateVImageVolume.hh"
 
-
-
 //-------------------------------------------------------------------------------------------------
-/// \brief Class of an actor that changes voxels densities during a simulation
+/* \brief Class of an actor that changes voxels densities during a simulation
+ *  
+ *  Script example :
+ *  /gate/actor/addActor     ImageDeformActor A
+ *  /gate/actor/A/attachTo   patient
+ *  /gate/actor/A/setPDFFile  data.txt
+ * 
+ * In data.txt :
+ * #Phase_time  CT_files
+ * 0.0    CT1.mhd
+ * 0.25   CT2.mhd
+ * 0.50   CT3.mhd
+ * 0.75   CT4.mhd
+ * 
+ **/
 class GateImageDeformActor  : public GateVActor
 {
 public:
@@ -50,57 +62,57 @@ public:
     \brief Set file used by the actor. It should contain 2 column : The time stamp and the CT file.
   */
   void SetFilename ( G4String filename );
-  void changeVolumeDensities ( G4double time );
-  void getVolumeList();
+//   void changeVolumeDensities ( G4double time );
+//   void getVolumeList();
 //   void Initialize();
 //   void SetInitialization(G4bool bool_ini);
   
   /*!
     \fn virtual void Construct();
-    \brief Function from GateVActor
+    \brief from GateVActor, it reads PDF file
   */
   virtual void Construct();
   
   /*!
     \fn virtual void BeginOfRunAction ( const G4Run* );
-    \brief Function from GateVActor. At the beginning of each run, the time is read, and compared the phases time (mTimeList). If necessary, CT densities are changed.
+    \brief from GateVActor. At the beginning of each run, the time is read, and compared the phases time (mTimeList). If necessary, CT densities are changed.
   */
   virtual void BeginOfRunAction ( const G4Run* ); 
   
   /*!
     \fn virtual void EndOfRunAction ( const G4Run* );
-    \brief Function from GateVActor. Default action (save)
+    \brief from GateVActor. Default action (save)
   */
   virtual void EndOfRunAction ( const G4Run* ); // default action (save)
   
   /*!
-    \fn virtual void BeginOfEventAction ( const G4Run* );
-    \brief Function from GateVActor. Default action 
+    \fn virtual void BeginOfEventAction ( const G4Event* );
+    \brief from GateVActor.
   */
   virtual void BeginOfEventAction ( const G4Event* ) ;
 //   virtual void EndOfEventAction(const G4Event*); // default action (save every n)
   
   /*!
-    \fn virtual void PreUserTrackingAction ( const G4Run* );
-    \brief Function from GateVActor. Default action 
+    \fn virtual void PreUserTrackingAction  ( const GateVVolume *, const G4Track* );
+    \brief from GateVActor. 
   */
   virtual void PreUserTrackingAction ( const GateVVolume *, const G4Track* ) {}
   
   /*!
-    \fn virtual void PostUserTrackingAction ( const G4Run* );
-    \brief Function from GateVActor. Default action 
+    \fn virtual void PostUserTrackingAction ( const GateVVolume *, const G4Track* );
+    \brief from GateVActor. 
   */
   virtual void PostUserTrackingAction ( const GateVVolume *, const G4Track* ) {}
   
   /*!
-    \fn virtual void UserSteppingAction ( const G4Run* );
-    \brief Function from GateVActor. Default action 
+    \fn virtual void UserSteppingAction ( const GateVVolume *, const G4Step* );
+    \brief from GateVActor.
   */
   virtual void UserSteppingAction ( const GateVVolume *, const G4Step* ) {}
 
   /*!
     \fn virtual void ResetData ( );
-    \brief Function from GateVActor. Default action 
+    \brief from GateVActor.
   */
   virtual void ResetData() {}
 
@@ -110,9 +122,9 @@ private :
   /*!
     \fn void readPDFFile ( G4String file );
     \param file Name of the file containing parameters
-    \brief Read the file parameters : time stamp and voxelized volumes.
+    \brief Read the file parameters : time stamps and voxelized volumes.
   */
-  void readPDFFile ( G4String file );
+  void readPDFFile ( G4String file );  // PDF : Probability density function
 
   GateClock *pClock;
   GateImageDeformActorMessenger* mMessenger;
