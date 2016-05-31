f9766973b07c38bb305d36583b431e87fe36cdf4
cenzhenjie <zhenjie.cen@creatis.insa-lyon.fr>
2016-05-31 10:11:58 +0200

    Update: * All changes in Geant4 are moved to Gate; * Refraction effects work for hard coding refractive index value.

---
+++ source/physics/include/G4XrayBoundaryProcess.hh
@@ -0,0 +1,145 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+// $Id: G4XrayBoundaryProcess.hh 84717 2014-10-20 07:39:47Z gcosmo $
+//
+//
+////////////////////////////////////////////////////////////////////////
+// X-ray Boundary Process Class Definition
+////////////////////////////////////////////////////////////////////////
+//
+// File:        G4XrayBoundaryProcess.hh
+// Description: Discrete Process -- reflection/refraction at
+//                                  interfaces
+// Version:     1.0
+// Created:     2016-05-26
+// Modified:
+//
+// Author:      Zhenjie Cen
+// mail:        zhenjie.cen@creatis.insa-lyon.fr
+//
+////////////////////////////////////////////////////////////////////////
+
+#ifndef G4XrayBoundaryProcess_h
+#define G4XrayBoundaryProcess_h 1
+
+/////////////
+// Includes
+/////////////
+
+#include "G4Step.hh"
+#include "G4VDiscreteProcess.hh"
+#include "G4EmProcessSubType.hh"
+#include "G4PhysicalConstants.hh"
+
+#include "G4ParallelWorldProcess.hh"
+#include "G4Gamma.hh"
+#include "G4TransportationManager.hh"
+
+// Class Description:
+// Discrete Process -- reflection/refraction at interfaces.
+// Class inherits publicly from G4VDiscreteProcess.
+// Class Description - End:
+
+/////////////////////
+// Class Definition
+/////////////////////
+
+class G4XrayBoundaryProcess : public G4VDiscreteProcess {
+
+ public:
+
+  ////////////////////////////////
+  // Constructors and Destructor
+  ////////////////////////////////
+
+  G4XrayBoundaryProcess(const G4String &processName = "XrayBoundary", G4ProcessType type = fElectromagnetic);
+  ~G4XrayBoundaryProcess();
+
+ private:
+
+  G4XrayBoundaryProcess(const G4XrayBoundaryProcess &right);
+  //////////////
+  // Operators
+  //////////////
+
+  G4XrayBoundaryProcess &operator=(const G4XrayBoundaryProcess &right);
+
+ public:
+
+  ////////////
+  // Methods
+  ////////////
+
+  G4bool IsApplicable(const G4ParticleDefinition &aParticleType);
+
+  G4double GetMeanFreePath(const G4Track &aTrack, G4double , G4ForceCondition *condition);
+  // Returns infinity; i. e. the process does not limit the step,
+  // but sets the 'Forced' condition for the DoIt to be invoked at
+  // every step. However, only at a boundary will any action be
+  // taken.
+
+  G4VParticleChange *PostStepDoIt(const G4Track &aTrack, const G4Step &aStep);
+  // This is the method implementing boundary processes.
+
+ private:
+
+  G4double GetIncidentAngle();
+
+ private:
+  G4double totalEnergy;
+  G4ThreeVector OldMomentum;
+  G4ThreeVector OldPolarization;
+
+  G4ThreeVector NewMomentum;
+  G4ThreeVector NewPolarization;
+
+  G4ThreeVector theGlobalNormal;
+
+  G4MaterialPropertyVector *PropertyPointer1;
+  G4MaterialPropertyVector *PropertyPointer2;
+
+  G4double Rindex1;
+  G4double Rindex2;
+
+  G4double cost1, cost2, sint1, sint2;
+
+  G4Material *Material1;
+  G4Material *Material2;
+
+};
+
+////////////////////
+// Inline methods
+////////////////////
+
+// For now, return true for all particles
+inline
+G4bool G4XrayBoundaryProcess::IsApplicable(const G4ParticleDefinition &aParticleType) {
+  return ( &aParticleType == G4Gamma::Gamma() );
+}
+
+#endif /* G4XrayBoundaryProcess_h */
---
+++ source/physics/src/G4XrayBoundaryProcess.cc
@@ -0,0 +1,219 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+////////////////////////////////////////////////////////////////////////
+// X-ray Boundary Process Class Implementation
+////////////////////////////////////////////////////////////////////////
+//
+// File:        G4XrayBoundaryProcess.cc
+// Description: Discrete Process -- reflection/refraction at
+//                                  interfaces
+// Version:     1.0
+// Created:     2016-05-26
+// Modified:
+//
+// Author:      Zhenjie Cen
+// mail:        zhenjie.cen@creatis.insa-lyon.fr
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "G4XrayBoundaryProcess.hh"
+
+
+/////////////////////////
+// Class Implementation
+/////////////////////////
+
+//////////////
+// Operators
+//////////////
+
+
+/////////////////
+// Constructors
+/////////////////
+G4XrayBoundaryProcess::G4XrayBoundaryProcess(const G4String &processName, G4ProcessType type) : G4VDiscreteProcess(processName, type) {
+  if (verboseLevel > 0) {
+    G4cout << GetProcessName() << " is created " << G4endl;
+  }
+  //SetProcessSubType(fXrayBoundary);
+}
+
+
+////////////////
+// Destructors
+////////////////
+G4XrayBoundaryProcess::~G4XrayBoundaryProcess() {}
+
+////////////
+// Methods
+////////////
+
+// PostStepDoIt
+// ------------
+//
+G4VParticleChange *G4XrayBoundaryProcess::PostStepDoIt(const G4Track &aTrack, const G4Step &aStep) {
+  // Get hyperStep from  G4ParallelWorldProcess
+  //  NOTE: PostSetpDoIt of this process should be
+  //        invoked after G4ParallelWorldProcess!
+
+  aParticleChange.Initialize(aTrack);
+  aParticleChange.ProposeVelocity(aTrack.GetVelocity());
+
+  const G4Step *pStep = &aStep;
+
+  const G4Step *hStep = G4ParallelWorldProcess::GetHyperStep();
+
+  if (hStep) pStep = hStep;
+
+  G4bool isOnBoundary =
+    (pStep->GetPostStepPoint()->GetStepStatus() == fGeomBoundary);
+
+  if (isOnBoundary) {
+    Material1 = pStep->GetPreStepPoint()->GetMaterial();
+    Material2 = pStep->GetPostStepPoint()->GetMaterial();
+  } else {
+    // if ( verboseLevel > 0) BoundaryProcessVerbose();
+    return G4VDiscreteProcess::PostStepDoIt(aTrack, aStep);
+  }
+
+  G4VPhysicalVolume *thePrePV  =
+    pStep->GetPreStepPoint() ->GetPhysicalVolume();
+  G4VPhysicalVolume *thePostPV =
+    pStep->GetPostStepPoint()->GetPhysicalVolume();
+
+  if ( verboseLevel > 0 || true ) {
+    G4cout << " X-ray at Boundary! " << G4endl;
+    if (thePrePV)  G4cout << " thePrePV:  " << thePrePV->GetName()  << G4endl;
+    if (thePostPV) G4cout << " thePostPV: " << thePostPV->GetName() << G4endl;
+  }
+
+  const G4DynamicParticle *aParticle = aTrack.GetDynamicParticle();
+
+  totalEnergy       = aParticle->GetTotalEnergy();
+  OldMomentum       = aParticle->GetMomentumDirection();
+  OldPolarization   = aParticle->GetPolarization();
+
+  if ( verboseLevel > 0 ) {
+    G4cout << " Old Momentum Direction: " << OldMomentum     << G4endl;
+    G4cout << " Old Polarization:       " << OldPolarization << G4endl;
+  }
+
+  G4ThreeVector theGlobalPoint = pStep->GetPostStepPoint()->GetPosition();
+
+  G4bool valid;
+  //  Use the new method for Exit Normal in global coordinates,
+  //    which provides the normal more reliably.
+
+  // ID of Navigator which limits step
+
+  G4int hNavId = G4ParallelWorldProcess::GetHypNavigatorID();
+  std::vector<G4Navigator *>::iterator iNav =
+    G4TransportationManager::GetTransportationManager()->
+    GetActiveNavigatorsIterator();
+  theGlobalNormal =
+    (iNav[hNavId])->GetGlobalExitNormal(theGlobalPoint, &valid);
+
+  if (valid) {
+    theGlobalNormal = -theGlobalNormal;
+  } else {
+    G4ExceptionDescription ed;
+    ed << " G4OpBoundaryProcess/PostStepDoIt(): "
+       << " The Navigator reports that it returned an invalid normal"
+       << G4endl;
+    G4Exception("G4OpBoundaryProcess::PostStepDoIt", "OpBoun01",
+                EventMustBeAborted, ed,
+                "Invalid Surface Normal - Geometry must return valid surface normal");
+  }
+
+  G4MaterialPropertiesTable *aMaterialPropertiesTable;
+  G4MaterialPropertyVector *Rindex;
+
+  aMaterialPropertiesTable = Material1->GetMaterialPropertiesTable();
+
+  if (aMaterialPropertiesTable) {
+    Rindex = aMaterialPropertiesTable->GetProperty("RINDEX");
+  } else {
+    // if ( verboseLevel > 0) BoundaryProcessVerbose();
+    return G4VDiscreteProcess::PostStepDoIt(aTrack, aStep);
+  }
+
+  PropertyPointer1 =
+          aMaterialPropertiesTable->GetProperty("REALRINDEX");
+  PropertyPointer2 =
+          aMaterialPropertiesTable->GetProperty("IMAGINARYRINDEX");
+
+  //G4cout << "REALRINDEX = " << PropertyPointer1->Value(totalEnergy) << G4endl;
+
+  G4double PdotN = OldMomentum * theGlobalNormal;
+  G4double EdotN = OldPolarization * theGlobalNormal;
+  cost1 = - PdotN;
+
+  G4double Rindex1 = thePrePV->GetName() == "Water" ? 1.33 : 1.00028;
+  G4double Rindex2 = thePostPV->GetName() == "Air" ? 1.00028 : 1.33;
+  if (std::abs(cost1) < 1.0) {
+    sint1 = std::sqrt(1. - cost1 * cost1);
+    sint2 = sint1 * Rindex1 / Rindex2; // *** Snell's Law ***
+  } else {
+    sint1 = 0.0;
+    sint2 = 0.0;
+  }
+
+  if (cost1 > 0.0) {
+    cost2 =  std::sqrt(1. - sint2 * sint2);
+  } else {
+    cost2 = -std::sqrt(1. - sint2 * sint2);
+  }
+
+  G4double alpha = cost1 - cost2 * (Rindex2 / Rindex1);
+  NewMomentum = OldMomentum + alpha * theGlobalNormal;
+  NewMomentum = NewMomentum.unit();
+
+  aParticleChange.ProposePolarization(OldPolarization);
+  aParticleChange.ProposeMomentumDirection(NewMomentum);
+
+
+
+  G4cout << "Incident angle: " << GetIncidentAngle() * 180 / pi << "refraction angle:" << std::asin(sint2) * 180 / pi << G4endl;
+  return G4VDiscreteProcess::PostStepDoIt(aTrack, aStep);
+}
+
+// GetMeanFreePath
+// ---------------
+//
+G4double G4XrayBoundaryProcess::GetMeanFreePath(const G4Track &, G4double , G4ForceCondition *condition) {
+  *condition = Forced;
+
+  return DBL_MAX;
+}
+
+G4double G4XrayBoundaryProcess::GetIncidentAngle() {
+  G4double PdotN = OldMomentum * theGlobalNormal;
+  G4double magP = OldMomentum.mag();
+  G4double magN = theGlobalNormal.mag();
+  G4double incidentangle = pi - std::acos(PdotN / (magP * magN));
+
+  return incidentangle;
+}
