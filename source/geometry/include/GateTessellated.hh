1c586da1150b42aaddd418b18b70d798b603a5eb
sjan <sebastien.jan@cea.fr>
2017-04-12 14:34:18 +0200

    New Tessellated Volume

---
+++ source/geometry/include/GateTessellated.hh
@@ -0,0 +1,54 @@
+/*----------------------
+  Copyright (C): OpenGATE Collaboration
+
+  This software is distributed under the terms
+  of the GNU Lesser General  Public Licence (LGPL)
+  See GATE/LICENSE.txt for further details
+  ----------------------*/
+
+#ifndef GateTessellated_h
+#define GateTessellated_h 1
+
+#include "GateVVolume.hh"
+#include "GateVolumeManager.hh"
+
+class G4TessellatedSolid;
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+
+class GateTessellatedMessenger;
+
+class GateTessellated : public GateVVolume
+{
+  public:
+    GateTessellated( G4String const& itsName, G4bool acceptsChildren = true, G4int depth = 0 );
+    GateTessellated( G4String const& itsName, G4String const& itsMaterialName );
+
+    virtual ~GateTessellated();
+
+    FCT_FOR_AUTO_CREATOR_VOLUME(GateTessellated)
+
+    virtual G4LogicalVolume* ConstructOwnSolidAndLogicalVolume( G4Material*, G4bool );
+    virtual void DestroyOwnSolidAndLogicalVolume();
+
+    // Declaration of pure virtual method
+    inline virtual G4double GetHalfDimension( size_t ) { return 0.; }
+
+    void SetPathToVerticesFile( G4String );
+
+  private:
+    void AddFacet( G4ThreeVector, G4ThreeVector, G4ThreeVector );
+
+  private:
+    G4TessellatedSolid*     m_tessellated_solid;
+    G4LogicalVolume*        m_tessellated_log;
+    G4VPhysicalVolume*      m_tessellated_phys;
+
+  private:
+    G4String m_PathToVerticesFile;
+    GateTessellatedMessenger* m_Messenger;
+};
+
+MAKE_AUTO_CREATOR_VOLUME(tessellated,GateTessellated)
+
+#endif
