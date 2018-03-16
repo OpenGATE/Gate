1c24b8802c0035aa4c4a7c1d77cab4b52606dba4
Ane Etxebeste <aetxebeste@tylopilus.clb.loc>
2018-03-16 16:36:18 +0100

    reader for a file with a  coincidence tree

---
+++ source/digits_hits/include/GateCoincidenceTreeReader.hh
@@ -0,0 +1,64 @@
+
+
+#ifndef GateCoincidenceTreeReader_h
+#define GateCoincidenceTreeReader_h 1
+
+#include "GateComptonCameraCones.hh"
+
+//root libraries , read Tree
+#include "TFile.h"
+#include "TTree.h"
+//necesario en Ubuntu
+#include "TSystem.h"
+//Los he tenido que incluir para el isZombie
+#include "TObject.h"
+#include "GateCCRootDefs.hh"
+
+
+
+class GateCoincidenceTreeReader
+{
+public:
+    GateCoincidenceTreeReader(const std::string path);
+     void PrepareAcquisition();
+      void LoadCoincData();
+     bool hasNext();
+     //GateComptonCameraCones getNext();
+    G4int PrepareNextEvent();
+    GateComptonCameraCones PrepareEndOfEvent();
+
+
+private:
+
+    const std::string filepath;
+
+
+      TFile * f;
+      TTree * m_coincTree;
+      int nentries;
+      int currentEntry;
+      int oldCoincID;
+
+
+      GateCCRootCoincBuffer       m_coincBuffer;
+
+
+      GateComptonCameraCones aCone;
+      int evtID, coinID;
+      double t;
+      float energy,posX,posY,posZ;
+      char lyrName[50];
+
+
+
+};
+
+#endif 
+
+
+
+
+
+
+
+
