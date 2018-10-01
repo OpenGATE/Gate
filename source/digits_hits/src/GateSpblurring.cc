fc9486fb9ba1b955750515912d707570d82d61ad
Ane Etxebeste <aetxebeste@tylopilus.clb.loc>
2018-10-01 15:23:06 +0200

    indentation

--- source/digits_hits/src/GateSpblurring.cc
+++ source/digits_hits/src/GateSpblurring.cc
@@ -20,8 +20,8 @@ See LICENSE.md for further details
 #include "GateConstants.hh"
 
 GateSpblurring::GateSpblurring(GatePulseProcessorChain* itsChain,
-			       const G4String& itsName,
-      	      	      	       G4double itsSpresolution)
+                   const G4String& itsName,
+                               G4double itsSpresolution)
   : GateVPulseProcessor(itsChain,itsName),
     m_spresolution(itsSpresolution)
 {
@@ -41,17 +41,17 @@ GateSpblurring::~GateSpblurring()
 void GateSpblurring::ProcessOnePulse(const GatePulse* inputPulse,GatePulseList& outputPulseList)
 {
 
-	GatePulse* outputPulse = new GatePulse(*inputPulse);
-	//TC G4ThreeVector P = inputPulse->GetGlobalPos();
-	G4ThreeVector P = inputPulse->GetVolumeID().MoveToBottomVolumeFrame(inputPulse->GetGlobalPos()); //TC
-	G4double Px = P.x();
-	G4double Py = P.y();
-	G4double Pz = P.z();
-	G4double PxNew = G4RandGauss::shoot(Px,m_spresolution/GateConstants::fwhm_to_sigma);
-	G4double PyNew = G4RandGauss::shoot(Py,m_spresolution/GateConstants::fwhm_to_sigma);
-	G4double PzNew = G4RandGauss::shoot(Pz,m_spresolution/GateConstants::fwhm_to_sigma); //TC
-	//TC G4double PzNew = Pz;
-    //AE
+    GatePulse* outputPulse = new GatePulse(*inputPulse);
+    //TC G4ThreeVector P = inputPulse->GetGlobalPos();
+    G4ThreeVector P = inputPulse->GetVolumeID().MoveToBottomVolumeFrame(inputPulse->GetGlobalPos()); //TC
+    G4double Px = P.x();
+    G4double Py = P.y();
+    G4double Pz = P.z();
+    G4double PxNew = G4RandGauss::shoot(Px,m_spresolution/GateConstants::fwhm_to_sigma);
+    G4double PyNew = G4RandGauss::shoot(Py,m_spresolution/GateConstants::fwhm_to_sigma);
+    G4double PzNew = G4RandGauss::shoot(Pz,m_spresolution/GateConstants::fwhm_to_sigma); //TC
+    //TC G4double PzNew = Pz;
+    //AE (Limits calculated for the bottom volume of the pulse) Maybe could  be done one for ecah volume and try to associate
     inputPulse->GetVolumeID().GetBottomCreator()->GetLogicalVolume()->GetSolid()->CalculateExtent(kXAxis, limits, at, Xmin, Xmax);
     inputPulse->GetVolumeID().GetBottomCreator()->GetLogicalVolume()->GetSolid()->CalculateExtent(kYAxis, limits, at, Ymin, Ymax);
     inputPulse->GetVolumeID().GetBottomCreator()->GetLogicalVolume()->GetSolid()->CalculateExtent(kZAxis, limits, at, Zmin, Zmax);
@@ -62,10 +62,10 @@ void GateSpblurring::ProcessOnePulse(const GatePulse* inputPulse,GatePulseList& 
     if(PyNew>Ymax) PyNew=Ymax;
     if(PzNew>Zmax) PzNew=Zmax;
     //
-	outputPulse->SetLocalPos(G4ThreeVector(PxNew,PyNew,PzNew)); //TC
-	outputPulse->SetGlobalPos(outputPulse->GetVolumeID().MoveToAncestorVolumeFrame(outputPulse->GetLocalPos())); //TC
-	//TC outputPulse->SetGlobalPos(G4ThreeVector(PxNew,PyNew,PzNew));
-	outputPulseList.push_back(outputPulse);
+    outputPulse->SetLocalPos(G4ThreeVector(PxNew,PyNew,PzNew)); //TC
+    outputPulse->SetGlobalPos(outputPulse->GetVolumeID().MoveToAncestorVolumeFrame(outputPulse->GetLocalPos())); //TC
+    //TC outputPulse->SetGlobalPos(G4ThreeVector(PxNew,PyNew,PzNew));
+    outputPulseList.push_back(outputPulse);
 
 }
 
