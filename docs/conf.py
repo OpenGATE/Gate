e96c1714f33ba561fe90ab89385babbf859f8482
tbaudier <thomas.baudier@creatis.insa-lyon.fr>
2019-04-15 14:22:54 +0200

    Fix the menu bar on the left
    When we scroll down the menu bas was moving and disappeared
    Now the menu bar is always at the top left

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+  'fixed_sidebar': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
