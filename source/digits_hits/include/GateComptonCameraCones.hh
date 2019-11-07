f5ff615afe0ef2a9757a0e1d74f1239dba7a72e9
anetxe <ane.etxebeste@creatis.insa-lyon.fr>
2019-11-07 11:48:14 +0100

    Adding info of the third interaction position (if there is one) and the energy of the second interaction

--- source/digits_hits/include/GateComptonCameraCones.hh
+++ source/digits_hits/include/GateComptonCameraCones.hh
@@ -10,6 +10,9 @@ public:
     inline void SetEnergy1(G4double de)          { m_E1 = de; }
     inline G4double GetEnergy1() const                { return m_E1; }
 
+    inline void SetEnergy2(G4double de)          { m_E2 = de; }
+    inline G4double GetEnergy2() const                { return m_E2; }
+
     inline void SetEnergyR(G4double de)          { m_ER = de; }
     inline G4double GetEnergyR() const                { return m_ER; }
 
@@ -21,6 +24,11 @@ public:
     inline void  SetPosition2(const G4ThreeVector& xyz)     { m_Pos2 = xyz; }
     inline const G4ThreeVector& GetPosition2() const             { return m_Pos2; }
 
+
+    inline void  SetPosition3(const G4ThreeVector& xyz)     { m_Pos3 = xyz; }
+    inline const G4ThreeVector& GetPosition3() const             { return m_Pos3; }
+
+
     inline void  SetTrueFlag(const G4bool& flag)     { m_IsTrueCoind = flag; }
      inline G4bool GetTrueFlag() const                { return m_IsTrueCoind; }
 
@@ -29,9 +37,11 @@ public:
 
 private:
   G4double m_E1;            // energy deposition of the first interaction
+  G4double m_E2;            // energy deposition of the second interaction
   G4double m_ER;            // Total energy deposition except E1
   G4ThreeVector m_Pos1;  //
   G4ThreeVector  m_Pos2;//  Second interaction
+  G4ThreeVector  m_Pos3;//  third interaction
   G4bool m_IsTrueCoind;
   G4int m_nSingles;
 
--- source/digits_hits/src/GateComptonCameraCones.cc
+++ source/digits_hits/src/GateComptonCameraCones.cc
@@ -2,6 +2,7 @@
 
 GateComptonCameraCones::GateComptonCameraCones()
  :m_E1(0.0),
+  m_E2(0.0),
   m_ER(0.0),
   m_IsTrueCoind(true),
   m_nSingles(0)
