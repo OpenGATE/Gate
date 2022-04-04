a35ba7ec1c174debed4084d443a7081b85c2951e
Christian Pommranz <christian@pommranz.de>
2022-04-04 09:42:52 +0200

    Remove old Geant4.9.6 compatibility switch

--- source/general/src/GateUIterminal.cc
+++ source/general/src/GateUIterminal.cc
@@ -12,11 +12,8 @@ See LICENSE.md for further details
 //LF
 #include "GateUIterminal.hh"
 
-//////////////////////////////////////////////////////
-//G4int GateUIterminal::ReceiveG4cout(G4String coutString)
-#ifdef Geant496_COMPATIBILITY
+
 G4int GateUIterminal::ReceiveG4cout(const G4String& coutString)
-//////////////////////////////////////////////////////
 {
   if ( coutString == "G4PhysicalVolumeModel::Validate() called.\n" )
     return 0;
@@ -30,26 +27,8 @@ G4int GateUIterminal::ReceiveG4cout(const G4String& coutString)
   std::cout << coutString << std::flush;
   return 0;
 }
-#else
-G4int GateUIterminal::ReceiveG4cout(G4String coutString)
-{
-  if ( coutString == "G4PhysicalVolumeModel::Validate() called.\n" )
-    return 0;
-  if ( coutString == "  Volume of the same name and copy number (\"world_P\", copy 0) still exists and is being used.\n" )
-    return 0;
-  if ( coutString == "  Be warned that this does not necessarily guarantee it's the same\n" )
-    return 0;
-  if ( coutString == "  volume you originally specified in /vis/scene/add/.\n" )
-    return 0;
 
-  std::cout << coutString << std::flush;
-  return 0;
-} 
-#endif
 
-//////////////////////////////////////////////////////
-//G4int GateUIterminal::ReceiveG4cerr(G4String cerrString)
-#ifdef Geant496_COMPATIBILITY
 G4int GateUIterminal::ReceiveG4cerr(const G4String& cerrString)
 {
 	std::cerr << "[G4-cerr] " <<  cerrString << std::flush;
@@ -73,11 +52,4 @@ G4int GateUIterminal::ReceiveG4cerr(const G4String& cerrString)
 
   return 0;
 }
-#else
-G4int GateUIterminal::ReceiveG4cerr(G4String cerrString)
-{
-  std::cerr << cerrString << std::flush;
-  return 0;
-}
-#endif
 
