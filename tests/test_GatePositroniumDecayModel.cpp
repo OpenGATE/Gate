8006a6dd46289d6136c9ec4ba236874a10003fa7
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2025-10-10 16:26:42 +0200

    Extend the test for GatePositroniumDecayModel

--- tests/test_GatePositroniumDecayModel.cpp
+++ tests/test_GatePositroniumDecayModel.cpp
@@ -1,13 +1,47 @@
 #include <cassert>
 #include <iostream>
+#include <vector>
 #include <GatePositroniumDecayModel.hh>
 
+/// It should be separated in several subtests
 bool run_tests()
 {
+  bool res = true;
   PositroniumDecayModelParams params;
   MiniPositroniumDecayModel model(params);
-  auto res = model.getPositroniumDecayParamsForEvent();
-  return true;
+  int index = MiniPositroniumDecayModel::getPositroniumDecayIndex({1});
+  if (index != 0) {
+    res = false;
+    std::cerr << "getPositroniumDecayDecayIndex({1})!=0" << std::endl;
+  }
+  index = MiniPositroniumDecayModel::getPositroniumDecayIndex({});
+  if (index != -1) {
+    res = false;
+    std::cerr << "getPositroniumDecayDecayIndex({})!=-1" << std::endl;
+  }
+
+  float epsilon = 1e-3;
+  int num_of_trials = 1e6;
+  std::vector<float> fractions={0.5,0.4,0.1};
+  std::vector<int> indices = {0,0,0};
+  for (int i = 0; i < num_of_trials; i++) {
+    index = MiniPositroniumDecayModel::getPositroniumDecayIndex(fractions);
+    indices[index] = indices[index] +1;
+  }
+  std::vector<float> estimated_fractions = {0.,0., 0.};
+  for (int i = 0; i < estimated_fractions.size(); i++) {
+    estimated_fractions[i] = float(indices[i])/num_of_trials;
+    std::cout << "estimated_fractions[i]="<<estimated_fractions[i]<< std::endl;
+  }
+
+  for (int i = 0; i < estimated_fractions.size(); i++) {
+    if(std::abs(fractions[i] - estimated_fractions[i])>epsilon)
+    {
+      res = false;
+      std::cerr << "assumed fractions and estimated fractions differ: std::abs(fractions[i] - estimated_fractions[i])" << std::endl;
+    }
+  }
+  return res;
 }
 
 int main()
