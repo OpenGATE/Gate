3dfd7ee50392d0723e2aac107f88fe28f4786d77
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2026-01-21 23:04:20 +0100

    Fix error in the GatePositroniumDecayModel constructor
    If decayKind was set to other than k2Gamma, then the type was always set
    to pPs.

--- source/physics/src/GatePositroniumDecayModel.cc
+++ source/physics/src/GatePositroniumDecayModel.cc
@@ -31,7 +31,7 @@ GatePositroniumDecayModel::GatePositroniumDecayModel(const PositroniumDecayModel
     {
       fPositroniumDecayChannel.push_back(std::move(GatePositronium("pPs", fModelParams.fLifetimes[i]* ns)));
     } else {
-      fPositroniumDecayChannel.push_back(std::move(GatePositronium("pPs", fModelParams.fLifetimes[i]* ns)));
+      fPositroniumDecayChannel.push_back(std::move(GatePositronium("oPs", fModelParams.fLifetimes[i]* ns)));
     }
   }
 }
