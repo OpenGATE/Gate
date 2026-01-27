d46f79c73e6c058f3ea3679e5036c23a6036dc73
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2026-01-27 22:15:35 +0100

    Add handling of command SetPositronInteractions
    It was removed accidentally while fixing the code.
    Also, change the arguments in macro to be consistent with the code
    naming e.g. kParaPs, kDirect, kOrthoPs.

--- source/physics/src/GatePositroniumSourceMessenger.cc
+++ source/physics/src/GatePositroniumSourceMessenger.cc
@@ -78,75 +78,85 @@ void GatePositroniumSourceMessenger::InitCommands()
  upCmdSetPositroniumFractions.reset(GetStringCmd( "setPositroniumFractions", "\"f1, f2, f3 .., fn\" - where fi in [0.0, 1.0] and sum of all fi ==1" ) );
  upCmdSetPositroniumLifetimes.reset(GetStringCmd( "setPositroniumLifetimes", "\"t1, t2, t3 .., tn\" - where ti corresponds to lifetime constants of the components" ) );
  upCmdSetDecayKinds.reset(GetStringCmd( "setDecayKinds", "\"k1, k2, k3 .., kn\" - where ki is k2Gamma or k3Gamma" ) );
- upCmdSetPositronInteractions.reset(GetStringCmd( "setPositronInteractions", "\"k1, k2, k3 .., kn\" - where ki is kpPs, kdirect or koPs, of a given element in vector of components. Used to properly recalculate intensities of the components from the theory" ) );
+ upCmdSetPositronInteractions.reset(GetStringCmd( "setPositronInteractions", "\"k1, k2, k3 .., kn\" - where ki is kParaPs, kDirect or kOrthoPs, of a given element in vector of components. Used to properly recalculate intensities of the components from the theory" ) );
  upCmdSetPromptPhotonProbabilites.reset(GetStringCmd( "setPromptPhotonProbabilites", "\"f1, f2, f3 .., fn\" - where fi in [0.0, 1.0] " ) );
  upCmdSetPromptPhotonEnergies.reset(GetStringCmd( "setPromptPhotonEnergies", "\"e1, e2, e3 .., fn\" - where ei are energies " ) );
 }
 
-void GatePositroniumSourceMessenger::SetNewValue( G4UIcommand* command, G4String new_value )
+void GatePositroniumSourceMessenger::SetNewValue(G4UIcommand *command, G4String new_value) 
 {
- if(command ==  upCmdSetPositroniumFractions.get())
- {
-  std::vector<float> fractions;
-  std::stringstream ss(new_value);
-  G4double num;
-  while (ss >> num) {
-    fractions.push_back(num);
-  }
-  fParamGenerator.SetPositroniumFraction(fractions);
- }
- else if(command ==  upCmdSetPositroniumLifetimes.get())
- {
-  std::vector<float> lifetimes;
-  std::stringstream ss(new_value);
-  G4double num;
-  while (ss >> num) {
-    lifetimes.push_back(num);
-  }
-  fParamGenerator.SetPositroniumLifetimes(lifetimes);
- }
- else if(command ==  upCmdSetPromptPhotonProbabilites.get())
- {
-   std::vector<float> promptPhotonProb;
-   std::stringstream ss(new_value);
-   float prob;
-   while (ss >> prob) {
-     prob = prob < 0 ? 0 : prob;
-     prob = prob > 1 ? 1 : prob;
-     promptPhotonProb.push_back(prob);
-   }
-   fParamGenerator.SetPromptGammaProbabilities(promptPhotonProb);
- }
- else if(command ==  upCmdSetPromptPhotonEnergies.get()) {
-  std::vector<float> promptPhotonEnergies;
-  std::stringstream ss(new_value);
-  float energy;
-  while (ss >> energy) {
-    promptPhotonEnergies.push_back(energy);
-  }
-  fParamGenerator.SetPromptGammaEnergies(promptPhotonEnergies);
- }
- else if(command ==  upCmdSetDecayKinds.get()) {
-  std::vector<PositroniumDecayKind> decayKinds;
-  std::stringstream ss(new_value);
-  std::string kind;
-  while (ss >> kind) {
-    if (kind == "k2Gamma")  {
+  if (command == upCmdSetPositroniumFractions.get()) {
+    std::vector<float> fractions;
+    std::stringstream ss(new_value);
+    G4double num;
+    while (ss >> num) {
+      fractions.push_back(num);
+    }
+    fParamGenerator.SetPositroniumFraction(fractions);
+  } else if (command == upCmdSetPositroniumLifetimes.get()) {
+    std::vector<float> lifetimes;
+    std::stringstream ss(new_value);
+    G4double num;
+    while (ss >> num) {
+      lifetimes.push_back(num);
+    }
+    fParamGenerator.SetPositroniumLifetimes(lifetimes);
+  } else if (command == upCmdSetPromptPhotonProbabilites.get()) {
+    std::vector<float> promptPhotonProb;
+    std::stringstream ss(new_value);
+    float prob;
+    while (ss >> prob) {
+      prob = prob < 0 ? 0 : prob;
+      prob = prob > 1 ? 1 : prob;
+      promptPhotonProb.push_back(prob);
+    }
+    fParamGenerator.SetPromptGammaProbabilities(promptPhotonProb);
+  } else if (command == upCmdSetPromptPhotonEnergies.get()) {
+    std::vector<float> promptPhotonEnergies;
+    std::stringstream ss(new_value);
+    float energy;
+    while (ss >> energy) {
+      promptPhotonEnergies.push_back(energy);
+    }
+    fParamGenerator.SetPromptGammaEnergies(promptPhotonEnergies);
+  } else if (command == upCmdSetDecayKinds.get()) {
+    std::vector<PositroniumDecayKind> decayKinds;
+    std::stringstream ss(new_value);
+    std::string kind;
+    while (ss >> kind) {
+      if (kind == "k2Gamma") {
         decayKinds.push_back(k2Gamma);
       } else {
         if (kind == "k3Gamma") {
           decayKinds.push_back(k3Gamma);
         } else {
-          GateError("GatePositroniumSourceMessenger::SetNewValue: unknown decay kind read from macro.");
+          GateError("GatePositroniumSourceMessenger::SetNewValue: unknown "
+                    "decay kind read from macro.");
         }
       }
+    }
+    fParamGenerator.SetDecayKinds(decayKinds);
+  } else if (command == upCmdSetPositronInteractions.get()) {
+    std::vector<PositronElectronInteraction> positronInteractions;
+    std::stringstream ss(new_value);
+    std::string inter;
+    while (ss >> inter) {
+      if (inter == "kParaPs") {
+        positronInteractions.push_back(PositronElectronInteraction::kParaPs);
+      } else if (inter == "kDirect") {
+        positronInteractions.push_back(PositronElectronInteraction::kDirect);
+      } else if (inter == "kOrthoPs") {
+        positronInteractions.push_back(PositronElectronInteraction::kOrthoPs);
+      } else {
+
+        GateError("GatePositroniumSourceMessenger::SetNewValue: unknown "
+                  "interaction type read from macro.");
+      }
+    }
+    fParamGenerator.SetPositronInteractions(positronInteractions);
+  } else {
+    GateVSourceMessenger::SetNewValue(command, new_value);
   }
-  fParamGenerator.SetDecayKinds(decayKinds);
- }
- else
- {
-  GateVSourceMessenger::SetNewValue(command, new_value);
- }
 }
 
 PositroniumDecayModelParams GatePositroniumSourceMessenger::generatePositroniumDecayParams(GatePositroniumDecayParamsGenerator::DecayModel model) const
