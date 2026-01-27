1424e874dbaa166c54533282aad0b04d8e4c3f59
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2026-01-27 20:30:42 +0100

    Remove obsolete comments

--- source/physics/include/GatePositroniumSource.hh
+++ source/physics/include/GatePositroniumSource.hh
@@ -17,31 +17,25 @@ class GatePositroniumSource : public GateVSource
 {
 public:
 
-  enum class ModelKind { 
-   NotDefined, //by default - in this case this class will behave like GateVSource
-   ParaPositronium, //generate gammas from para-positronium decay
-   OrthoPositronium, //generate gammas from ortho-positronium decay
-   Positronium}; // generate gammas from mixed model
+  enum class ModelKind {
+    NotDefined,
+    ParaPositronium,
+    OrthoPositronium,
+    Positronium
+  };
 
   explicit GatePositroniumSource(const G4String& name);
   virtual ~GatePositroniumSource() = default;
 
-  /** Generate gammas for event
-   **/
   virtual G4int GeneratePrimaries( G4Event* event ) override;
 
  protected:
-  /** Set model used for this source. If is not defined then this class will behave like GateVSource.
-   **/
    void SetModel(const G4String &model_name);
-   /** Prepare model to work - set all settings from user to model
-    **/
    void PrepareModel();
 
  protected:
-   std::unique_ptr<GateGammaEmissionModel> pModel;
-   std::unique_ptr<GatePositroniumSourceMessenger> pMessenger;
-  //User settings:
+  std::unique_ptr<GateGammaEmissionModel> pModel;
+  std::unique_ptr<GatePositroniumSourceMessenger> pMessenger;
   ModelKind fModelKind = ModelKind::NotDefined;
 };
 
