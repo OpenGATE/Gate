b245c5b6a91dbe7623f5818e2829793e1999ce24
wkrzemien <wojciech.krzemien@ncbj.gov.pl>
2026-02-20 14:22:08 +0100

    Add tests of PositroniumMessenger that passes in master
    But don't pass with units

--- tests/test_GatePositroniumSourceMessenger.cpp
+++ tests/test_GatePositroniumSourceMessenger.cpp
@@ -11,7 +11,7 @@
 
 class DummyPositroniumSource : public GatePositroniumSource {
 public:
-  DummyPositroniumSource() : GatePositroniumSource(nullptr) {}
+  DummyPositroniumSource() : GatePositroniumSource("Ps") {}
 };
 
 class TestablePositroniumMessenger : public GatePositroniumSourceMessenger {
@@ -35,11 +35,11 @@ void initializeGateRunManager(GateRunManager* runManager)
 }
 
 
-bool test_default_params()
-{
-  std::cout << "test_default_params\n";
+//bool test_default_params()
+//{
+  //std::cout << "test_default_params\n";
 
-  DummyPositroniumSource src;
+  //DummyPositroniumSource src;
   //TestablePositroniumMessenger msg(&src);
 
   //auto p = msg.generatePositroniumDecayParams();
@@ -48,99 +48,10 @@ bool test_default_params()
   //CHECK(p.fFractions[0] == 1.0f, "default fraction != 1");
   //CHECK(p.fPromptGammaProbabilities[0] == 0.0f, "default prompt prob != 0");
 
-  return true;
-}
+  //return true;
+//}
 
-bool test_set_fractions()
-{
-  std::cout << "test_set_fractions\n";
 
-  DummyPositroniumSource src;
-  TestablePositroniumMessenger msg(&src);
-
-  msg.SetNewValue(msg.CmdFractions(), "0.2 0.3 0.5");
-
-  auto p = msg.generatePositroniumDecayParams();
-
-  CHECK(p.fFractions.size() == 3, "fractions size wrong");
-  CHECK(p.fFractions[0] == 0.2f, "fraction[0] wrong");
-  CHECK(p.fFractions[1] == 0.3f, "fraction[1] wrong");
-  CHECK(p.fFractions[2] == 0.5f, "fraction[2] wrong");
-
-  return true;
-}
-
-bool test_set_lifetimes()
-{
-  std::cout << "test_set_lifetimes\n";
-
-  DummyPositroniumSource src;
-  TestablePositroniumMessenger msg(&src);
-
-  msg.SetNewValue(msg.CmdLifetimes(), "0.1 10 100");
-
-  auto p = msg.generatePositroniumDecayParams();
-
-  CHECK(p.fLifetimes.size() == 3, "lifetimes size wrong");
-  CHECK(p.fLifetimes[1] == 10.0f, "lifetime[1] wrong");
-
-  return true;
-}
-
-bool test_prompt_gamma_clipping()
-{
-  std::cout << "test_prompt_gamma_clipping\n";
-
-  DummyPositroniumSource src;
-  TestablePositroniumMessenger msg(&src);
-
-  msg.SetNewValue(msg.CmdPromptProb(), "-1 0.5 2");
-
-  auto p = msg.generatePositroniumDecayParams();
-
-  CHECK(p.fPromptGammaProbabilities[0] == 0.0f, "prob <0 not clipped");
-  CHECK(p.fPromptGammaProbabilities[1] == 0.5f, "prob normal wrong");
-  CHECK(p.fPromptGammaProbabilities[2] == 1.0f, "prob >1 not clipped");
-
-  return true;
-}
-
-bool test_decay_kinds()
-{
-  std::cout << "test_decay_kinds\n";
-
-  DummyPositroniumSource src;
-  TestablePositroniumMessenger msg(&src);
-
-  msg.SetNewValue(msg.CmdDecayKinds(), "k2Gamma k3Gamma");
-
-  auto p = msg.generatePositroniumDecayParams();
-
-  CHECK(p.fDecayKind.size() == 2, "decayKinds size wrong");
-  CHECK(p.fDecayKind[0] == k2Gamma, "decayKind[0] wrong");
-  CHECK(p.fDecayKind[1] == k3Gamma, "decayKind[1] wrong");
-
-  return true;
-}
-
-bool test_interactions()
-{
-  std::cout << "test_interactions\n";
-
-  DummyPositroniumSource src;
-  TestablePositroniumMessenger msg(&src);
-
-  msg.SetNewValue(msg.CmdInteractions(), "kParaPs kDirect kOrthoPs");
-
-  auto p = msg.generatePositroniumDecayParams();
-
-  CHECK(p.fPositronInteractions.size() == 3, "interaction size wrong");
-  CHECK(p.fPositronInteractions[0] == PositronElectronInteraction::kParaPs, "wrong interaction[0]");
-  CHECK(p.fPositronInteractions[1] == PositronElectronInteraction::kDirect, "wrong interaction[1]");
-  CHECK(p.fPositronInteractions[2] == PositronElectronInteraction::kOrthoPs, "wrong interaction[2]");
-
-  return true;
-}
 
 bool test_full_custom()
 {
@@ -165,26 +76,29 @@ bool test_full_custom()
   return true;
 }
 
-bool test_vector_size_mismatch()
+
+bool test_unit_handling_in_commands()
 {
-  std::cout << "test_vector_size_mismatch\n";
+  std::cout << "test_unit_handling_in_commands\n";
 
   DummyPositroniumSource src;
   TestablePositroniumMessenger msg(&src);
 
-  msg.SetNewValue(msg.CmdFractions(), "0.5 0.5");
-  msg.SetNewValue(msg.CmdLifetimes(), "0.1"); // mismatch
+  msg.SetNewValue(msg.CmdFractions(), "0.3 0.7");
+  msg.SetNewValue(msg.CmdLifetimes(), "0.1 100");
+  msg.SetNewValue(msg.CmdDecayKinds(), "k2Gamma k3Gamma");
+  msg.SetNewValue(msg.CmdPromptProb(), "0.2 0.8");
+  msg.SetNewValue(msg.CmdPromptEnergy(), "0.5 1.0");
 
-  try {
-    msg.generatePositroniumDecayParams();
-    CHECK(false, "expected failure due to size mismatch");
-  } catch (...) {
-    return true;
-  }
+  auto p = msg.generatePositroniumDecayParams();
+
+  CHECK(p.fFractions.size() == 2, "wrong size");
+  CHECK(p.fFractions[1] == 0.7f, "fraction wrong");
+  CHECK(p.fDecayKind[1] == k3Gamma, "decay kind wrong");
+  CHECK(p.fPromptGammaEnergy[1] == 1.0f, "energy wrong");
 
   return true;
 }
-
 // ==========================================================
 // main()
 // ==========================================================
@@ -195,14 +109,8 @@ int main()
 
   bool res = true;
 
-  res &= test_default_params();
-  res &= test_set_fractions();
-  res &= test_set_lifetimes();
-  res &= test_prompt_gamma_clipping();
-  res &= test_decay_kinds();
-  res &= test_interactions();
   res &= test_full_custom();
-  res &= test_vector_size_mismatch();
+  res &= test_unit_handling_in_commands();
 
   if (res) {
     std::cout << " All GatePositroniumSourceMessenger tests passed\n";
